ae85b58bcce22b33a9f14473f1910424c11cd170
jsackett <jeffrey.sackett@itron.com>
2021-02-19 14:01:06 -0700

    Create the application

---
+++ lib/widget_market.ex
@@ -0,0 +1,18 @@
+defmodule WidgetMarket do
+  @moduledoc """
+  Documentation for `WidgetMarket`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WidgetMarket.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/widget_market_test.exs
@@ -0,0 +1,8 @@
+defmodule WidgetMarketTest do
+  use ExUnit.Case
+  doctest WidgetMarket
+
+  test "greets the world" do
+    assert WidgetMarket.hello() == :world
+  end
+end
